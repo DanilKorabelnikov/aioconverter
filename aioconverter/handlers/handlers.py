24bb4edea2badadefea1f3cfd91d2062b80acd67
DanilKorabelnikov <71430053+DanilKorabelnikov@users.noreply.github.com>
2020-09-26 23:23:43 +0300

    fix bug

--- aioconverter/handlers/handlers.py
+++ aioconverter/handlers/handlers.py
@@ -1,8 +1,9 @@
 async def currency(curr: str):
 
-	currency = ("EURRUB", "RUBEUR", "USDRUB", "RUBUSD", "BTCRUB", "RUBBTC", "BTCEUR", "EURBTC", "USDBTC","BTCUSD")
+	currency = ("EURRUB", "RUBEUR", "USDRUB", "RUBUSD", "BTCRUB", "RUBBTC", "BTCEUR", "EURBTC", "USDBTC", "BTCUSD")
 
-	if curr.lower() not in currency:
-		raise TypeError("Unknown currency index!")
-	else:
-		return curr.lower()
+	for i in currency:
+		if i.lower() not in currency:
+			raise TypeError("Unknown currency index!")
+		else:
+			return i.lower()
