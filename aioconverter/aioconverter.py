58f3d2ef53ae16d61ea7565b58a67ce42b5624c1
DanilKorabelnikov <71430053+DanilKorabelnikov@users.noreply.github.com>
2020-09-26 23:31:33 +0300

    fix bug

--- aioconverter/aioconverter.py
+++ aioconverter/aioconverter.py
@@ -1,9 +1,6 @@
-from typing import Optional
-
 from aioconverter.const import const
 from aioconverter.http.http import request
 from aioconverter.types.models import Model
-from aioconverter.handlers.handlers import currency_get
 
 
 class Currency:
@@ -13,12 +10,12 @@ class Currency:
 		"""
 		self._token = token
 
-	async def converter(self, pairs: Optional[str] = 'USDRUB') -> Model:
+	async def converter(self, pairs: str = 'USDRUB') -> Model:
 		response = await request(
 				const.currency_api, {
 					'key': self._token,
 					'get': 'rates',
-					'pairs': currency_get(curr=pairs)
+					'pairs': pairs
 				}
 		)
 		return Model(**response)
--- aioconverter/handlers/handlers.py
+++
@@ -1,8 +0,0 @@
-def currency_get(curr: str):
-
-	currency = ("EURRUB", "RUBEUR", "USDRUB", "RUBUSD", "BTCRUB", "RUBBTC", "BTCEUR", "EURBTC", "USDBTC", "BTCUSD")
-
-	if curr.lower() not in currency:
-		raise TypeError("Unknown currency index!")
-	else:
-		return curr.lower()
